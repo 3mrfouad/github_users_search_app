a8aa2099da0f68f6c41756760695e0bd7fd08676
Amr Fouad <3mrfouad@gmail.com>
2022-05-03 10:34:07 -0600

    create `Instructions` component - It provides basic instructions on how to use the app

---
+++ src/components/Instructions.js
@@ -0,0 +1,13 @@
+export default function Instructions() {
+  return (
+    <div className={'instructions'}>
+      <p>
+        <strong>Hello, and Welcome!</strong>
+      </p>{' '}
+      To get started, please enter a user or organization name in the search
+      box, Then choose a type as <strong>Users</strong> or{' '}
+      <strong>Organizations</strong> then click the <strong>Search</strong>{' '}
+      button.
+    </div>
+  )
+}
