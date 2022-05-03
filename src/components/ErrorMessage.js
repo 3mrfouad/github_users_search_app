a9c8afec982dbdfb83918fb664cbaa8cdc2ac02b
Amr Fouad <3mrfouad@gmail.com>
2022-05-03 16:51:58 -0600

    use the correct google icon for error

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -2,7 +2,7 @@ export default function ErrorMessage({ message }) {
   return (
     message && (
       <div className={'errorMsgContainer'}>
-        <span className={'material-symbols-outlined'}>info</span>
+        <span className={'material-symbols-outlined'}>error</span>
         <span>{message}</span>
       </div>
     )
