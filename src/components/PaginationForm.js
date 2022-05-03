c766007dbb5f8e560e6f6758c5fba7387d0bc843
Amr Fouad <3mrfouad@gmail.com>
2022-05-03 13:43:38 -0600

    create `Pagination Form` component - it renders when there are more than one page of search results

---
+++ src/components/PaginationForm.js
@@ -0,0 +1,82 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// @ts-nocheck
+import { useEffect, useState } from 'react'
+import { PER_PAGE_SIZE } from '../helpers/constants'
+export default function PaginationForm({
+  results,
+  perPage,
+  setPerPage,
+  page,
+  setPage,
+  handleSubmit
+}) {
+  const [showPagination, setShowPagination] = useState(false)
+  const [submitEvent, setSubmitEvent] = useState()
+  const [pagesTotal, setPagesTotal] = useState(0)
+
+  useEffect(() => {
+    setShowPagination(
+      results &&
+        results?.total_count > Math.min(...Object.values(PER_PAGE_SIZE)) &&
+        results?.total_count >= results?.items?.length
+    )
+    setPagesTotal(Math.ceil(results?.total_count / perPage))
+  }, [results, perPage])
+
+  useEffect(() => {
+    results && handleSubmit(submitEvent)
+  }, [page, perPage])
+
+  return (
+    showPagination && (
+      <form className="paginationForm">
+        <label className={'perPage'}>
+          Results per page{' '}
+          <select
+            value={perPage}
+            onChange={e => {
+              setPerPage(e.target.value)
+              setSubmitEvent(e)
+            }}
+          >
+            {Object.values(PER_PAGE_SIZE).map(num => {
+              return (
+                <option key={num} value={num}>
+                  {num}
+                </option>
+              )
+            })}
+          </select>
+        </label>
+
+        <div className={'btnGroup'}>
+          <button
+            type={'submit'}
+            disabled={page === 1}
+            onClick={e => {
+              setPage(page - 1)
+              setSubmitEvent(e)
+            }}
+            className={'submitBtn paginationBtn'}
+          >
+            Previous
+          </button>
+          <span className={'pageCount'}>
+            {page}/{pagesTotal}
+          </span>
+          <button
+            type={'submit'}
+            disabled={page >= pagesTotal}
+            onClick={e => {
+              setPage(page + 1)
+              setSubmitEvent(e)
+            }}
+            className={'submitBtn paginationBtn'}
+          >
+            Next
+          </button>
+        </div>
+      </form>
+    )
+  )
+}
---
+++ src/helpers/constants.js
@@ -0,0 +1,6 @@
+export const PER_PAGE_SIZE = Object.freeze({
+  TEN: 10,
+  THIRTY: 30,
+  FIFTY: 50,
+  HUNDRED: 100
+})
