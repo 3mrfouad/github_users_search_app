f5b04bf9c692094efcec49d27647e1da50fd8b36
Amr Fouad <3mrfouad@gmail.com>
2022-05-03 14:37:46 -0600

    implement components styling with darkmode enabled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
-import githubLogo from '../assets/github.png'
 export default function Header() {
   return (
     <header className={'header'}>
-      <img src={githubLogo} role={'presentation'} alt={''} className={'logo'} />
+      <img role={'presentation'} alt={''} className={'logo'} />
       <p>Github Search App</p>
     </header>
   )
