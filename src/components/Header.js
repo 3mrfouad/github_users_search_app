d23df1ba454b25aa18c7c4afd660ddf24797be97
Amr Fouad <3mrfouad@gmail.com>
2022-05-03 15:26:22 -0600

    add alt text to images in `Header` and `Loader`

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className={'header'}>
-      <img src={''} role={'presentation'} alt={''} className={'logo'} />
+      <img src={''} alt={'Github logo'} className={'logo'} />
       <h2>Github Search App</h2>
     </header>
   )
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,7 +4,7 @@ export default function Loader({ loading }) {
   return (
     loading && (
       <div className={'loader'}>
-        <img src={spinner} role={'presentation'} alt={''} />
+        <img src={spinner} alt={'Loading search results'} />
       </div>
     )
   )
