8d83b823993e20eccc222376899af8a87b89d38f
Amr Fouad <3mrfouad@gmail.com>
2022-05-04 02:07:39 -0600

    add count to front page

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,20 +1,22 @@
-import { useLayoutEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Card from './Card'
 export default function SearchResults({ results, perPage, type }) {
   const [showSearchResults, setShowSearchResults] = useState(false)
   const [usersList, setUsersList] = useState([])
   const [title, setTitle] = useState('')
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     setShowSearchResults(results?.items && results?.items?.length !== 0)
     setUsersList(results?.items?.slice(0, perPage) || [])
     setTitle(type === 'org' ? 'Organization: ' : 'Users: ')
-  }, [results, perPage])
+  }, [results, perPage, type])
 
   return (
     showSearchResults && (
       <div className={'resultsContainer'}>
-        <h1>{title}</h1>
+        <h1>
+          {title} <span className="count">Count: {results?.items?.length}</span>
+        </h1>
         <ul className={'listContainer'}>
           {usersList.map((result, i) => (
             <li className={'listItem'} key={i}>
